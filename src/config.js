5728fecd48a7b906ab6bc1d4eae6d4ba6c286cb3
AgusFleitas <ezequiel-agustin-.-@hotmail.com>
2024-07-11 17:28:37 +0200

    Add production credentials

--- src/config.js
+++ src/config.js
@@ -5,3 +5,4 @@ export const PORT = 3000;
 export const HOST = "http://localhost:" + PORT;
 
 export const ACCESS_TOKEN_TEST = process.env.ACCESS_TOKEN_TEST;
+export const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
